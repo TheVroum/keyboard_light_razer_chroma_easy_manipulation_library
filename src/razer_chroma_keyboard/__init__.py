de050a3bec5beda6c68aa2c8ea6d4f69735d6e6f
TheVroum <36685559+TheVroum@users.noreply.github.com>
2023-01-03 12:08:22 +0100

    first release

--- src/razer_chroma_keyboard/__init__.py
+++ src/razer_chroma_keyboard/__init__.py
@@ -1,6 +1,6 @@
 """Copyright © 2023 Jean Oustry. Tous droits réservés.
 """
-import time, threading, itertools
+import time
 from .api import razerServerChromaConnection
 
 __all__ = [
@@ -8,59 +8,10 @@ __all__ = [
 ]
 
 
-
-def example_mooving_highlight():
-    class Point:
-        def __init__(self):
-            self._col = 0
-            self._row = 0
-        @property
-        def col(self):
-            return self._col
-        @col.setter
-        def col(self, value):
-            self._col = min(21, max(0, value))
-        @property
-        def row(self):
-            return self._row
-        @row.setter
-        def row(self, value):
-            self._row = min(5, max(0, value))
-
-    actualize = threading.Event()
-    actualize.set()
-
-    coord = Point()
-    actions = {pynput.keyboard.Key.left: lambda: coord.__setattr__("col", coord.__getattribute__("col") - 1)
-            , pynput.keyboard.Key.up: lambda: coord.__setattr__("row", coord.__getattribute__("row") - 1)
-            , pynput.keyboard.Key.right: lambda: coord.__setattr__("col", coord.__getattribute__("col") + 1)
-            , pynput.keyboard.Key.down: lambda: coord.__setattr__("row", coord.__getattribute__("row") + 1)}
-    callback = lambda key: [actions[key]() if key in actions else None, actualize.set()]
-
-    pynput.keyboard.Listener(on_press=callback).start()
-
-    with razerServerChromaConnection() as connection:
-        while True:
-            actualize.wait()
-            actualize.clear()
-            connection.generate_key_highlight_chroma_custom(coord.col, coord.row)
-
-
 def example_random_highlight():
     with razerServerChromaConnection() as connection:
         while True:
             connection.apply_rand_chroma_custom()
-            time.sleep(0.3)
-
-
-def example_new_api():
-    c = [[(0, 0, 255) if _ >= 22/2 else (0, 255, 0) for _ in range(22)] for __ in range(6)]
-    for i in itertools.product(range(22), range(6)):
-        if i[0] == 0 or  i[0] == 21 or  i[1] == 0 or i[1] == 5:
-            c[i[1]][i[0]] = (255, 0, 0)
-    with razerServerChromaConnection() as connection:
-        connection.apply_chroma_custom(c)
-        while True:
             time.sleep(0.3)
 
 
